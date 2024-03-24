d67cfe6701163104d18a03799d75e9d33477e58f
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-03-24 21:53:44 +0100

    Agregando componente User a App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UserComponent} from "./user/user.component";
+import {CommonModule} from "@angular/common";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
