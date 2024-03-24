2e97900dec92da702aa131a777eb3aaf26f7c594
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-03-24 18:25:51 +0100

    Comentarios y ejemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+/*DECORADOR: Describe como va a ser nuestro componente*/
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', /*La forma en que nos referimos al componente*/
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', /*lo que renderiza*/
   styleUrl: './app.component.css'
 })
+/*APLICA MAGIA A ESTA CLASE APPCOMPONENT*/
 export class AppComponent {
   title = 'my-price-app';
+  city = 'Barcelona';
 }
+/*Stadalone --> significa que lo pondremos en imports, lo que queremos usar en este componente*/
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,3 +6,8 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+
+/*En Angular, los "providers" son objetos que Angular utiliza para resolver dependencias y
+ proporcionar instancias de servicios, valores o cualquier otro objeto que tu aplicación necesite.
+ Los providers le dicen a Angular cómo obtener o crear instancias
+de una clase cuando se solicitan a través de la inyección de dependencias.*/
