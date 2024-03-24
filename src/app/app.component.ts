82913432d19dce437d248a4ec79e55632c55f774
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-03-24 18:32:09 +0100

    Retirar comentarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-/*DECORADOR: Describe como va a ser nuestro componente*/
 @Component({
-  selector: 'app-root', /*La forma en que nos referimos al componente*/
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html', /*lo que renderiza*/
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-/*APLICA MAGIA A ESTA CLASE APPCOMPONENT*/
+
 export class AppComponent {
   title = 'my-price-app';
   city = 'Barcelona';
 }
-/*Stadalone --> significa que lo pondremos en imports, lo que queremos usar en este componente*/
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-/*Punto de entrada de nuestra aplicación*/
-/*Cargame la aplicación y lo renderizas*/
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
